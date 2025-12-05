4648bd21771a9abf1600dbcd35f84f2837ced8fa
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-12-05 19:44:41 +0100

    cambios

--- obj/Debug/net8.0/EntregaAlex.AssemblyInfo.cs
+++ obj/Debug/net8.0/EntregaAlex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntregaAlex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7504eddee991c644820a8357ef094af42bbac53f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9755ccb641c1400ef50dcb20f48e2bf6e33097ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntregaAlex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntregaAlex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
