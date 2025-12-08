cfbbdf7851f74624cbe58a93e5a8cacd266ee815
Alecove <a28306@svalero.com>
2025-12-08 22:31:12 +0100

    Codigo final corregido con Docker y base de datos limpia

--- obj/Debug/net8.0/EntregaAlex.AssemblyInfo.cs
+++ obj/Debug/net8.0/EntregaAlex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntregaAlex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4c9199a3735c3daa447a826d77ca3e9d3a78fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c16e84620b5318cca7104bdab85f25488f9e10b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntregaAlex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntregaAlex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
